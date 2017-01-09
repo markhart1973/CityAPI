7f4e42f67853e45be8507fe8a7fb1f310128438a
markhart1973 <markhart1973@hotmail.com>
2017-01-09 13:03:26 +0000

    Added support for XML

--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -5,8 +5,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
 
 namespace CityInfo.API
 {
@@ -16,7 +18,18 @@ namespace CityInfo.API
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc()
+                .AddMvcOptions(o => o.OutputFormatters.Add(
+                    new XmlDataContractSerializerOutputFormatter()));
+                //.AddJsonOptions(o =>
+                //{
+                //    if (o.SerializerSettings.ContractResolver != null)
+                //    {
+                //        var resolver = o.SerializerSettings.ContractResolver as DefaultContractResolver;
+                //        resolver.NamingStrategy = null;
+                //    }
+                //});
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
